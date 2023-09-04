108f499ff574628474b75d0587d3325718f7659b
TriNM <trisnm102@gmail.com>
2023-09-04 16:24:43 +0700

    Add home page and init Benefit page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
 import Navbar from "@/scenes/navbar";
-import { useState } from "react";
+import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
+import { useEffect, useState } from "react";
+import  { SelectedPage } from "@/shared/types";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<string>('home')
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      } else {
+        setIsTopOfPage(false);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-      <div className="app bd-gray-20">
+      <div className="app bg-gray-20">
         <Navbar 
+          isTopOfPage={isTopOfPage}
           selectedPage={selectedPage}
           setSelectedPage={setSelectedPage}
         />
+
+        <Home setSelectedPage={setSelectedPage} />
+        <Benefits setSelectedPage={setSelectedPage} />
       </div>
   )
 }
---
+++ src/scenes/benefits/index.tsx
@@ -0,0 +1,62 @@
+import { SelectedPage, BenefitType } from "@/shared/types"
+import HText from "@/shared/HText"
+import Benefit from "@/scenes/benefits"
+import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid"
+import { motion } from "framer-motion";
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void;
+}
+
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the Art Facilities",
+    description:
+      "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description:
+      "Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id.",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description:
+      "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.",
+  },
+];
+
+const Benefits = ({setSelectedPage}: Props) => {
+  return (
+    <section
+      id = "benefits"
+      className="mx-auto min-h-full w-5/6 py-20"
+    >
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
+        <div className="md:my-5 md:w-3/5">
+          <HText>MORE THAN JUST GYM. </HText>
+          <p className="my-5 text-sm">
+            We provide world class fitness equipment, trainers and classes to
+            get you to your ultimate fitness goals with ease. We provide true
+            care into each and every member.
+          </p>
+        </div>
+
+      {/* benefits section */}
+      <div className="mt-5 items-center justify-between gap-8 md:flex">
+        {benefits.map(() => (
+          <Benefit />
+        ))}
+      </div>
+      
+      </motion.div>
+    </section>
+  )
+}
+
+export default Benefits
